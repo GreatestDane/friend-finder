c264c9acee5d05319b5ca5b44003e95530be8ce5
Greatest Dane <DaneGoodman85@gmail.com>
2018-07-22 19:41:52 -0700

    I think I've gone as far with this thing as I can

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,105 @@
-var friends = {
-    name:"Ahmed",
-  photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  scores:[
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
-      2,
-      5,
-      4,
-      1
+
+//List of potential matches
+var myAPI = [{
+    "name": "Ahmed",
+    "photo": "https://c1.staticflickr.com/8/7175/6698567177_fc5df89f18_b.jpg",
+    "scores": [
+        3,
+        2,
+        1,
+        1,
+        2,
+        5,
+        2,
+        3,
+        2,
+        1
+    ]
+},
+{
+    "name": "becky", 
+    "photo": "https://dz9yg0snnohlc.cloudfront.net/new-the-enjoyable-art-of-learning-to-talk-to-random-people-2.png",
+    "scores": [
+        5,
+        5,
+        3,
+        3,
+        1,
+        1,
+        4,
+        5,
+        2,
+        3
+
     ]
 
 },
+{
+    "name": "Jenny",
+    "photo": "https://www.rd.com/wp-content/uploads/2017/03/22-People-Share-the-Random-Act-of-Kindness-That-Changed-Their-Life-Polly-380x254.jpg",
+    "scores": [
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+    ]
+},
+{
+    "name": "Demarcus",
+    "photo": "https://vignette.wikia.nocookie.net/althistory/images/7/74/Recent-portraits-random-people-in-random-places_11.jpg/revision/latest?cb=20141209232111",
+    "scores": [
+        3,
+        2,
+        5,
+        1,
+        4,
+        3,
+        5,
+        1,
+        2,
+        2
+    ]
+},
+{
+    "name": "Peter",
+    "photo": "http://pm1.narvii.com/5866/76b201e12343e860570c62f4067307e04ed545fe_00.jpg",
+    "scores": [
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
+    ]
+},
+{
+    "name": "Sarah",
+    "photo": "https://image.slidesharecdn.com/random-people-preview-160918064958/95/random-people-6-638.jpg?cb=1474788136",
+    "scores": [
+        4,
+        3,
+        1,
+        2,
+        5,
+        4,
+        5,
+        3,
+        2,
+        5
+    ]
+}
+
+];
+
+
+
--- server.js
+++ server.js
@@ -1,20 +1,35 @@
 //Require essential packages
 var express = require("express");
-
 var bodyParser = require("body-parser");
 var path = require("path");
+
+
 //Set up the server to listen on port 3000
 var app = express();
 var PORT = process.env.PORT || 3000;
+
 //set up body parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static('public'));
+
+//Create the HTML routes
 app.get('/', function (req, res) {
-    res.sendFile('home.html', { root: __dirname + "./public" } );
+    res.sendFile('home.html', { root: __dirname + "/public" } );
+    
+  });
+
+  app.get('/survey', function (req, res) {
+    res.sendFile('survey.html', { root: __dirname + "/public" } );
+    
+  });
+
+  app.get('/api/friends', function (req, res) {
+    res.sendFile('friends.js', { root: __dirname + "/app/data" } );
     
   })
+
 
 // Starts the server to begin listening
 // =============================================================
